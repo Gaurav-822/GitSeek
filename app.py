7176eba7f75d4f18f06a37eb770d726d32943e6d
Gaurav Bhushan Kumar <gaurav.moocs@gmail.com>
2023-10-29 01:25:48 +0530

    .

--- app.py
+++ app.py
@@ -6,22 +6,6 @@ import json
 # Configuring Flask Application
 app = Flask(__name__)
 
-# Ensure templates are auto reloded
-app.config["TEMPLATES_AUTO_RELOD"] = True
-
-# Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-
-@app.after_request
-def after_request(response):
-    """Ensure responses aren't cached"""
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
-
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
